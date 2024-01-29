006c4e67e5a33407d40d1e490cb826ccb080802c
Enyell Duarte <engell.sof.vall@gmail.com>
2024-01-28 22:32:48 -0400

    Se amplio el diccionario

--- poema.js
+++ poema.js
@@ -1,8 +1,8 @@
 // Listas de palabras ampliadas
-let sujetos = ["Mis pensamientos", "Mi corazón", "Mis sueños", "Mi amor", "Mis días", "Mis noches", "Mis deseos", "Mis esperanzas"];
-let verbos = ["anhelan", "buscan", "admiran", "aman", "desean", "sueñan con", "necesitan", "aprecian"];
-let objetos = ["tu dulzura", "tu sonrisa", "tu mirada", "tu esencia", "tu risa", "tu voz", "tu amabilidad", "tu inteligencia"];
-let adverbios = ["apasionadamente", "tiernamente", "profundamente", "sinceramente", "devotamente", "fervientemente", "constantemente", "eternamente"];
+let sujetos = ["Mis pensamientos", "Mi corazón", "Mis sueños", "Mi amor", "Mis días", "Mis noches", "Mis deseos", "Mis esperanzas", "Mis recuerdos", "Mis emociones", "Mis sentimientos", "Mis anhelos", "Mis lágrimas", "Mis risas", "Mis suspiros", "Mis silencios", "Mis palabras", "Mis secretos", "Mis miedos", "Mis alegrías"];
+let verbos = ["anhelan", "buscan", "admiran", "aman", "desean", "sueñan con", "necesitan", "aprecian", "valoran", "contemplan", "anhelan", "extrañan", "respetan", "recuerdan", "sienten", "escuchan", "ven", "tocan", "saben", "entienden"];
+let objetos = ["tu dulzura", "tu sonrisa", "tu mirada", "tu esencia", "tu risa", "tu voz", "tu amabilidad", "tu inteligencia", "tu paciencia", "tu generosidad", "tu fortaleza", "tu valentía", "tu ternura", "tu pasión", "tu alma", "tu luz", "tu sombra", "tu calor", "tu frío", "tu silencio"];
+let adverbios = ["apasionadamente", "tiernamente", "profundamente", "sinceramente", "devotamente", "fervientemente", "constantemente", "eternamente", "incondicionalmente", "silenciosamente", "tranquilamente", "alegremente", "tristemente", "serenamente", "rápidamente", "lentamente", "fuertemente", "suavemente", "claramente", "oscuramente"];
 
 // Función para obtener un elemento aleatorio de una lista
 function obtenerAleatorio(lista) {
